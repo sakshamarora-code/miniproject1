7c140d075ab8f92d18377d66b4249b68e1578b0a
sakshamarora-code <sakshamarora75@gmail.com>
2020-04-13 18:48:30 +0530

    Performed Step 3

--- rps.py
+++ rps.py
@@ -4,6 +4,10 @@ print("Winning Rules of the Rock Paper Scissor Game as follows: \n"
                                 +"\tRock  vs scissor -> Rock wins \n"
                                 +"\tpaper vs scissor -> scissor wins \n")
 while True:
+    # Tell User to enter either Rock, Paper, Scissor
+    print("Enter choice \n 1. rock \n 2. paper \n 3. scissor \n")
+    user_choice = input("User turn: > ")
+    print("User has choosen : " + user_choice + "\n")
     ans = input("Do you want to play again ? (Y/N) > ")
     if (ans == 'n' or ans == 'N') :
         break
