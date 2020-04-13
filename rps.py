cd64534b18d2adac53621fa104503e3eee403d34
sakshamarora-code <sakshamarora75@gmail.com>
2020-04-13 18:38:02 +0530

    Performed Step 1

---
+++ rps.py
@@ -0,0 +1,8 @@
+# Print the Game Instruction to the User/Player
+print("Winning Rules of the Rock Paper Scissor Game as follows: \n"
+                                +"\tRock  vs Paper   -> Paper wins \n"
+                                +"\tRock  vs Scissor -> Rock wins \n"
+                                +"\tPaper vs Scissor -> Scissor wins \n")
+
+
+print("\nThanks for playing Rock Paper Scissor Game \n")
